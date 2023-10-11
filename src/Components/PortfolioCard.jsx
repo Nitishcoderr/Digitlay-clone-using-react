57c82112e335c125c9275dde468c7e584e29fe3e
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-10-11 11:35:19 +0530

    lightbox added

--- src/Components/PortfolioCard.jsx
+++ src/Components/PortfolioCard.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import '../Styles/Portfolio.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import {SlideshowLightbox,initLightboxJS} from 'lightbox.js-react'
+import 'lightbox.js-react/dist/index.css'
+import { useEffect } from 'react';
 
 const PortfolioCard = ({projectData}) => {
   AOS.init();
+
+
+  useEffect(() => {
+    initLightboxJS("work");
+  }, []);
   return (
     <>
 
@@ -16,7 +24,9 @@ const PortfolioCard = ({projectData}) => {
                     <div data-aos="zoom-in"
                     data-aos-easing="ease-out-cubic"
                     data-aos-duration="3000" key={id} className="pro-card">
-                    <img  src={image} className="card-img-top" alt="img"/>
+                      <SlideshowLightbox className="container grid grid-cols-3 gap-2 mx-auto">
+                      <img className="w-full rounded" src={image} alt='work' />
+                      </SlideshowLightbox> 
                       <p className='project-about'>{about}</p>
                     </div>
                 )
